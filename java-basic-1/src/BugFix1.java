20bce0d18825c1be593f534a67710df5ebb6078d
ruha321 <105205825+ruha321@users.noreply.github.com>
2025-04-18 13:49:28 +0900

    javaの基礎1、目標B

---
+++ java-basic-1/src/BugFix1.java
@@ -0,0 +1,8 @@
+public class BugFix1 {
+    public static void main(String[] args) {
+        System.out.println("Hello! \"木村君\" \n ");
+        System.out.print("A");
+        System.out.print("B");
+        System.out.print("C");
+    }
+}
