359123261aee802e0e3124be619f5eb2ff88af6a
ruha321 <105205825+ruha321@users.noreply.github.com>
2025-04-24 15:32:14 +0900

    javaの基礎1、課題2 レベルA

--- java-basic-1/src/TheBoilingEarthEra.java
+++ java-basic-1/src/TheBoilingEarthEra.java
@@ -6,7 +6,7 @@ public class TheBoilingEarthEra {
         Random random = new Random();
         for (int i = 0; i < temperatures.length; i++) {
             for (int j = 0; j < temperatures[i].length; j++) {
-                temperatures[i][j] = random.nextDouble() + 29.0 + i * 0.3;
+                temperatures[i][j] = random.nextDouble() * 10 - 5 + 29.0 + i * 0.3;
             }
         }
     }
---
+++ java-basic-1/src/TheBoilingEarthEraA.java
@@ -0,0 +1,19 @@
+public class TheBoilingEarthEraA extends TheBoilingEarthEra {
+
+    int year2025 = 2025-2016;
+    public static void main(String[] args) {
+        TheBoilingEarthEraA era = new TheBoilingEarthEraA();
+        era.printTemperatures();
+    }
+    public TheBoilingEarthEraA() {
+        super();
+    }
+
+    public void printTemperatures() {
+        for (int i = 0; i < temperatures[year2025].length; i++) {
+            System.out.printf("2025年7月%d日: %.2f℃\n", i + 1, temperatures[year2025][i]);
+        }
+    }
+    
+
+}
