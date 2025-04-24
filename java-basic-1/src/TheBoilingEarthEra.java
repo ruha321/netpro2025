acf6c6ef244ce3e467dc719a7633e5f7eb5fcc24
ruha321 <105205825+ruha321@users.noreply.github.com>
2025-04-24 15:19:20 +0900

    javaの基礎1、課題2javaファイル名の変更とコンストラクタの作成

---
+++ java-basic-1/src/TheBoilingEarthEra.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class TheBoilingEarthEra {
+    double[][] temperatures = new double[10][31];
+
+    TheBoilingEarthEra() {
+        Random random = new Random();
+        for (int i = 0; i < temperatures.length; i++) {
+            for (int j = 0; j < temperatures[i].length; j++) {
+                temperatures[i][j] = random.nextDouble() + 29.0 + i * 0.3;
+            }
+        }
+    }
+
+}
