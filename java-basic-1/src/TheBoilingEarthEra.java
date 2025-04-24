6bf2b05eeddc4acf17b4eafc01985b1487524f41
ruha321 <105205825+ruha321@users.noreply.github.com>
2025-04-24 18:53:01 +0900

    javaの基礎1、課題2:実際のデータを使用するように修正

--- java-basic-1/src/TheBoilingEarthEra.java
+++ java-basic-1/src/TheBoilingEarthEra.java
@@ -1,14 +1,39 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.*;
 public class TheBoilingEarthEra {
     double[][] temperatures = new double[10][31];
 
+    final int CSV_YEAR = 0;
+    final int CSV_DAY = 2;
+    final int CSV_TEMPERATURE = 3;
+
+
     TheBoilingEarthEra() {
-        Random random = new Random();
-        for (int i = 0; i < temperatures.length; i++) {
-            for (int j = 0; j < temperatures[i].length; j++) {
-                temperatures[i][j] = random.nextDouble() * 10 - 5 + 29.0 + i * 0.3;
+        // 2016年から2024年までの気温データをアメダスのdata.csvファイルから格納
+        try {
+            Scanner sc = new Scanner(new FileInputStream("data.csv"));
+            // CSVファイルのヘッダーをスキップ
+            for (int i = 0; i < 6; i++) {
+                sc.nextLine();
             }
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] data = line.split(",");
+                int year = Integer.parseInt(data[CSV_YEAR]) - 2016;
+                temperatures[year][Integer.parseInt(data[CSV_DAY]) - 1] = Double.parseDouble(data[CSV_TEMPERATURE]);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+
+        // 2025年はデータが無いためランダムな気温を生成
+        for (int i = 0; i < temperatures[9].length; i++) {
+            Random random = new Random();
+            temperatures[9][i] = random.nextDouble() * 10 - 5 + 31.7;
+        }
+        
     }
 
 }
